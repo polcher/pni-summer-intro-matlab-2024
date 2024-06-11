47773e8c1373219aa6cdc98983fd1a2dadc27f6c
IamPolina <38703666+IamPolina@users.noreply.github.com>
2024-06-11 13:45:17 -0400

    Add files via upload

---
+++ Week 1/week_2_tues_workbook.m
@@ -0,0 +1,35 @@
+%% Arrays 
+% write down an array of numbers (using variable naming guidelines)
+
+% write down an string of characters
+
+% write down a collection consisting your array and your string
+
+%% Matrices
+% Write down the matrix from the slide
+
+% Select 7 and 10 from this matrix using row and column indexing
+
+% Select [4,8,12] from this matrix using row and column indexing
+
+
+
+% Select 7 from this matrix using one index (and then select 10 with one index)
+
+%% 3 Dimensional matrices 
+% Write down a 3 dimensional matrix
+
+% Select 3
+
+% Select [11,12,13]
+
+% Select [3,6]
+
+%% Define two matrices, concatenate and multiply
+
+% Create a 3X4 nans matrix and rand matrix and concatenate them in row dimension. 
+% Create a 3X4 ones matrix and randi matrix and concatenate them in row dimension. 
+% Multiply the matrices element by element.
+
+
+
