9d5d433ccd9eda3d5b1f9010a992b10f0b5a4cc1
IamPolina <38703666+IamPolina@users.noreply.github.com>
2024-06-18 13:54:54 -0400

    Add files via upload

---
+++ Week 3/week_3_tues_workbook.m
@@ -0,0 +1,29 @@
+%% Exercise 1
+% 1. Create an array of integers going from 20 to 1 with a step of 2. 
+% 2. Find what integer goes under index 4. 
+% 3. Find an index of integer 4. 
+
+
+%% Exercise 2
+% 1. create a 3X4 matrix using rand.
+% 2. find indices of the elements more or equal to 0.5
+% 3. replace these elements with nan.
+
+
+% * replace all the numbers in your matrix which are bigger than 0.5 with
+% 100. Do not use find. 
+
+
+%% Exercise 3
+% create a matrix of 10X10 randi up to value 10.
+% find indices of elements bigger than 3 and smaller than 7. 
+% find indices of elements either smaller than 2 or bigger than 9. 
+% make it a function receiving a matrix as an input and output two arrays
+% of indices. 
+
+%% Exercise 4
+% 1. How to test if at least any of the elements in a matrix are not 0? 
+%2. How to test if all the elements in a matrix are not 0? 
+
+
+
