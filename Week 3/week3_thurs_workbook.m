12395ad31d6edd165721f8548433776d81519801
polcher <158093184+polcher@users.noreply.github.com>
2024-06-20 13:27:22 -0400

    Add files via upload

---
+++ Week 3/week3_thurs_workbook.m
@@ -0,0 +1,40 @@
+%% Exercise 1
+% Generate a 4-element array of random integers. 
+
+% Write an if statement that returns "The sum is ___ and it is 
+% greater than 20" if the sum of the array is greater than 20, 
+% or “The sum is ___ and it is less than 20” if the sum of the 
+% array is less than 20.
+
+
+%% Exercise 2
+% Generate a 4-element array of random integers. 
+
+% % Write an if statement that returns "The sum is ___ and it is 
+% greater than 20" if the sum of the array is greater than 20, 
+% “The sum is ___ and it is less than 20” if the sum of the 
+% array is less than 20, or "The sum is 20" if the sum of the array
+% is 20.
+
+%% Exercise 3
+% Generate a 5-element array of random integers.
+
+% Write a while loop where for each element of the array, if the 
+% element is greater than 5 or equal to 1, return the element.
+
+
+%% Exercise 4
+% Rewrite your code from Exercise 3, but using a for loop.
+
+
+%% Exercise 5
+% Generate two random 3x4 matrices consisting of integers
+% between -6 and 6.
+
+
+
+% Write a nested for loop that creates a new 3x4 matrix called 
+% `sum_matrix` that contains the sum of the two matrices. 
+
+
+
